d81d819d9b4d897bc853d4c3dcbfe36b3542e881
KarRobin <robindebx@gmail.com>
2020-04-08 11:13:17 +0200

    exo 19

--- exo_19.rb
+++ exo_19.rb
@@ -6,5 +6,14 @@ a = "john.weak" + "#{n}" + "@killkitten.io"
 mails.push(a)
 n+=1
 end
-a = mails.select()
-print a
+
+
+puts mails.select.each_with_index { |wallou, index| index.odd? }
+
+
+# Test, pas prendre en compte
+puts mails.class
+puts n.class
+puts each.class
+
+a.reverse
